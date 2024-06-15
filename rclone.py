be377ea771c51c007b72148b793c4472e47dd698
mrspidy <74917158+MrxAravind@users.noreply.github.com>
2024-06-15 18:38:54 +0530

    Update rclone.py

--- rclone.py
+++ rclone.py
@@ -1,4 +1,4 @@
 import os
-os.system("sudo -v ; curl https://rclone.org/install.sh | sudo bash")
+os.system(" -v ; curl https://rclone.org/install.sh | bash")
 os.system("wget https://gist.github.com/MrxAravind/cff83e9fa0d8b3f627e4d049c893b776/raw/3eec18861e0635b54ad50191c0fea91b5f339e86/rclone.conf")
 os.system("rclone --config rclone.conf serve webdav --addr :80")
